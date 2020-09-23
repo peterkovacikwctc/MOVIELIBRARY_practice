fe84dfbbb350c573c261de82e4c1fa492f9b809d
Peter Kovacik <pkovacik2@my.wctc.edu>
2020-09-23 18:30:20 -0500

    make sure movies.csv file exists

--- Program.cs
+++ Program.cs
@@ -13,7 +13,19 @@ namespace MovieLibrary_practice
             // create instance of Logger
             var logger = NLogBuilder.ConfigureNLog(path).GetCurrentClassLogger();
             logger.Info("Program started");
-            Console.WriteLine("Hello World!");
+            
+            string file = "movies.csv";
+             // make sure movie file exists
+            if (!File.Exists(file))
+            {
+                logger.Error("File does not exist: {File}", file);
+            }
+            else
+            {
+                // TODO: create user menu
+            }
+
+
 
              logger.Info("Program ended");
         }
