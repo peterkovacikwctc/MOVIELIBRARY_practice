322c1292be7a0a2ef5f9ddea24206c310e9979a1
Peter Kovacik <pkovacik2@my.wctc.edu>
2020-09-23 19:14:25 -0500

    save data to file

--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using NLog.Web;
 using System.IO;
-using System.Collections.Generic; // namespace needed to use lists
+using System.Collections.Generic;
 using System.Linq;
 
-namespace MovieLibrary_practice
+namespace MovieListing
 {
     class Program
     {
@@ -15,7 +15,7 @@ namespace MovieLibrary_practice
             // create instance of Logger
             var logger = NLogBuilder.ConfigureNLog(path).GetCurrentClassLogger();
             logger.Info("Program started");
-            
+
             string file = "movies.csv";
              // make sure movie file exists
             if (!File.Exists(file))
@@ -33,7 +33,7 @@ namespace MovieLibrary_practice
                 try
                 {
                     StreamReader sr = new StreamReader(file);
-                    // skip first line -- contains column headers
+                    // first line contains column headers
                     sr.ReadLine();
                     while (!sr.EndOfStream)
                     {
@@ -43,7 +43,7 @@ namespace MovieLibrary_practice
                         int idx = line.IndexOf('"');
                         if (idx == -1)
                         {
-                           // no quote = no comma in movie title
+                            // no quote = no comma in movie title
                             // movie details are separated with comma(,)
                             string[] movieDetails = line.Split(',');
                             // 1st array element contains movie id
@@ -77,11 +77,12 @@ namespace MovieLibrary_practice
                 {
                     logger.Error(ex.Message);
                 }
+
                 string choice;
                 do
                 {
                     logger.Info("Movies in file {Count}", MovieIds.Count);
-                    
+
                     // display choices to user
                     Console.WriteLine("1) Add Movie");
                     Console.WriteLine("2) Display All Movies");
@@ -108,7 +109,7 @@ namespace MovieLibrary_practice
                         {
                             // generate movie id - use max value in MovieIds + 1
                             UInt64 movieId = MovieIds.Max() + 1;
-                           // input genres
+                            // input genres
                             List<string> genres = new List<string>();
                             string genre;
                             do
@@ -133,14 +134,22 @@ namespace MovieLibrary_practice
                             string genresString = string.Join("|", genres);
                             // if there is a comma(,) in the title, wrap it in quotes
                             movieTitle = movieTitle.IndexOf(',') != -1 ? $"\"{movieTitle}\"" : movieTitle;
-                            // display movie id, title, genres
-                            Console.WriteLine($"{movieId},{movieTitle},{genresString}");
+                             // create file from data
+                            StreamWriter sw = new StreamWriter(file, true);
+                            sw.WriteLine($"{movieId},{movieTitle},{genresString}");
+                            sw.Close();
+                            // add movie details to Lists
+                            MovieIds.Add(movieId);
+                            MovieTitles.Add(movieTitle);
+                            MovieGenres.Add(genresString);
+                            // log transaction
+                            logger.Info("Movie id {Id} added", movieId);
                         }
                     }
                     else if (choice == "2")
                     {
                         // Display All Movies
-                         // loop thru Movie Lists
+                        // loop thru Movie Lists
                         for (int i = 0; i < MovieIds.Count; i++)
                         {
                             // display movie details
@@ -152,10 +161,8 @@ namespace MovieLibrary_practice
                     }
                 } while (choice == "1" || choice == "2");
             }
-
-
-
-             logger.Info("Program ended");
+            
+            logger.Info("Program ended");
         }
     }
 }
